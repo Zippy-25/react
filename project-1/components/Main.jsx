7f756adc068a14797300ab3dc8cc22daca34ef31
Darshana R <jaidarsh1983@gmail.com>
2026-03-30 10:57:54 +0530

    Create Main.jsx

---
+++ project-1/components/Main.jsx
@@ -0,0 +1,14 @@
+export default function Main(){
+    return(
+        <main>a
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2003</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
